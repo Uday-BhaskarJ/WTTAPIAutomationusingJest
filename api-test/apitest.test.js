065fb619af8d0d3b19b9e206e38a8a4b2dac9284
Shruti Goyal <shruti.goyal@mhealth.tech>
2022-07-18 14:45:51 +0530

    CRUD of reqres API uisng JEST

--- api-test/apitest.test.js
+++ api-test/apitest.test.js
@@ -15,12 +15,14 @@ describe('user manage test suit',()=>{
      test.each([1,2,3])('GET-> all users from page (%s)', async(page)=>{
         const response = await request.get(`api/users?page=${page}`)
         console.log('Recevied all users',response.body)
+
         expect(response.status).toBe(200)
      });
 
      test.each(Array.from(Array(5).keys()))('GET-> all users from page (%s)', async(page)=>{
         const response = await request.get(`api/users?page=${page}`)
         console.log('Recevied all users',response.body)
+
         expect(response.status).toBe(200)
      });
 
@@ -32,6 +34,7 @@ describe('user manage test suit',()=>{
          const response = await request.post('api/users').send(user)
          job = response.body.job
          console.log('New user created:',response.body)
+
          expect(response.body.job).toBe("SDET")
          expect(response.status).toBe(201)
      });
@@ -44,6 +47,7 @@ describe('user manage test suit',()=>{
          const response = await request.post('api/users').send(user)
          job = response.body.job
          console.log('New user created:',response.body)
+
          expect(response.body.job).toBe("SDET")
          expect(response.status).toBe(201)
      });
@@ -56,6 +60,7 @@ describe('user manage test suit',()=>{
          const response = await request.post('api/users').send(user)
          job = response.body.job
          console.log('New user created:',response.body)
+
          expect(response.body.job).toBe(`${job}`)
          expect(response.status).toBe(201)
      });
@@ -67,6 +72,7 @@ describe('user manage test suit',()=>{
                           }
          const response = await request.put('api/users/2').send(updateUser)
          console.log('Updated user:',response.body)
+
          expect(response.body.name).toBe('Utkarsh')
          expect(response.body.job).toBe(`${job}`)
          expect(response.status).toBe(200)
@@ -74,7 +80,8 @@ describe('user manage test suit',()=>{
 
      test('DELETE-> delete the single user', async()=>{
         const response = await request.delete('api/users/2')
-        expect(response.status).toBe(204)
-        console.log('Deleted the user:',response.body)
+         console.log('Deleted the user:',response.body)
+
+         expect(response.status).toBe(204)
      });
 });
